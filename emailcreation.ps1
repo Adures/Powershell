eb476a7efebc5aea2e08232e93140360a5c1ec59
Adures <github@lagit.pl>
2021-03-28 19:06:16 +0200

    commit

---
+++ emailcreation.ps1
@@ -0,0 +1,70 @@
+Import-module ExchangeOnlineManagement
+
+$path = "C:\Apps\adminemail.txt"
+if (Test-Path $path) {
+    $Adminemail = Get-Content $Path
+   # Connect-MsolService 
+     Connect-ExchangeOnline -userPrincipalNAme $Adminemail -ShowProgress $True
+    
+} else {
+  $Adminemail = Read-Host "Please type your eol admin email, you see this line only because you are using this script from a new device"
+  $Adminemail | out-file "C:\Apps\adminemail.txt"
+   
+    #Connect-MsolService 
+     Connect-ExchangeOnline -userPrincipalNAme $Adminemail -ShowProgress $True
+     
+}
+
+
+fucntion createmailbox ($mailbox) {
+
+    $emailmaiblox = $mailbox
+
+
+
+}
+
+function displaynamechoice {
+
+  Read-Host = "1. To display as Adrian Stefaniak"
+  Read-Host = "2. To display as Adures"
+  Read-Host = "To display as emai for example wordpress@lagit.pl displays as Wordpress"
+  Read-Host = 
+
+}
+
+
+function Main-Menu {
+    param (
+        [Parameter(Mandatory=$false)]
+        [string]$Title = 'Menu'
+    )
+    changescreensize 30 60
+    Clear-Host
+    Start-Sleep -Milliseconds 200
+
+    #add new menu entry, adjust switches + functions below menu section
+Write-Host "================== $Title ==================="  -BackgroundColor DarkGreen
+   
+Write-Host "1: Check and change mfa status"
+Write-Host "2: Generate report"
+
+
+$selection = Read-Host "Please make a selection"
+     switch ($selection)
+     {
+         '1' {
+
+            $mailbox = read-host "Please provide an email address for the mailbox that you want to create"
+            
+            createmailbox $mailbox 
+             
+         } '2' {
+           Main-Menu
+         } 
+      }
+
+
+
+}
+main-menu
